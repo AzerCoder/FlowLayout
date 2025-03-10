7074cd9580ef289aac363bfc778ba22c8a777e21
A'zamjon Abdumuxtorov <azamjonabdumuxtorov1220@gmail.com>
2025-03-10 15:25:12 +0500

    Initial Commit

---
+++ FlowLayout/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FlowLayout
+//
+//  Created by A'zamjon Abdumuxtorov on 10/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FlowLayout/FlowLayoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowLayoutApp.swift
+//  FlowLayout
+//
+//  Created by A'zamjon Abdumuxtorov on 10/03/25.
+//
+
+import SwiftUI
+
+@main
+struct FlowLayoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
